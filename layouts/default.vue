dbbbe92b71086dee2f64f533a272322f68e6b117
Fabricio-Guima <fsgkof@gmail.com>
2022-06-26 16:40:26 -0300

    #loading e desabilitar botoes

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,13 +8,7 @@
       app
     >
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -24,18 +18,10 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-        class="mr-2"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      <v-btn icon @click.stop="miniVariant = !miniVariant" class="mr-2">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
       </v-btn>
       <!-- <v-btn
         icon
@@ -43,53 +29,37 @@
       >
         <v-icon>mdi-application</v-icon>
       </v-btn> -->
-     
+
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <client-only>
-        <v-btn text color="white" :to="{name: 'minha-assinatura'}">Minha assinatura</v-btn>
-        <v-btn
-          v-if="$auth.loggedIn"
-         
-          @click="logout"
-       
+        <v-btn text v-if="$auth.loggedIn" :to="{ name: 'minha-assinatura' }"
+          >Minha assinatura</v-btn
         >
-          Encerrar
-        </v-btn>
+        <v-btn v-if="$auth.loggedIn" @click="logout"> Encerrar </v-btn>
 
-      <div v-if="!$auth.loggedIn">
-        <v-btn text color="white" :to="{name: 'login'}">Inciar Sessão</v-btn>
-        <v-btn text color="white" :to="{name: 'cadastro'}">Cadastrar-se</v-btn>
-      </div>
+        <div v-if="!$auth.loggedIn">
+          <v-btn text :to="{ name: 'login' }">Inciar Sessão</v-btn>
+          <v-btn text :to="{ name: 'cadastro' }">Cadastrar-se</v-btn>
+        </div>
       </client-only>
-     
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
       <v-list>
         <v-list-item @click.native="right = !right">
           <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
+            <v-icon light> mdi-repeat </v-icon>
           </v-list-item-action>
           <v-list-item-title>Switch drawer (click me)</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -97,50 +67,48 @@
 
 <script>
 export default {
-  name: 'DefaultLayout',
-  data () {
+  name: "DefaultLayout",
+  data() {
     return {
       clipped: true,
       drawer: false,
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Home',
-          to: '/'
+          icon: "mdi-apps",
+          title: "Home",
+          to: "/",
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Favoritos',
-          to: '/inspire'
-        },
-         {
-          icon: 'mdi-chart-bubble',
-          title: 'Planos',
-          to: '/planos'
+          icon: "mdi-chart-bubble",
+          title: "Favoritos",
+          to: "/inspire",
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Faturas',
-          to: '/faturas'
-        }
+          icon: "mdi-chart-bubble",
+          title: "Planos",
+          to: "/planos",
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Faturas",
+          to: "/faturas",
+        },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Imóveis'
-    }
+      title: "Imóveis",
+    };
   },
   methods: {
     async logout() {
-      await this.$auth.logout()
+      await this.$auth.logout();
 
-      this.$router.push('/login')
-      console.log('logout')
-    }
+      this.$router.push("/login");
+      console.log("logout");
+    },
   },
-  created() {
-
-  }
-}
+  created() {},
+};
 </script>
--- pages/minha-assinatura.vue
+++ pages/minha-assinatura.vue
@@ -2,34 +2,80 @@
   <div>
     <h1>Minha assinatura</h1>
     <pre>
-        {{$auth.user}}
+        {{ $auth.user }}
     </pre>
 
-   
-     <v-btn  v-if="$auth.user.onGracePeriod" depressed color="success" @click="reactivate">Reativar assinatura</v-btn>     
-     <v-btn  v-if="$auth.user.isSubscribed && !$auth.user.onGracePeriod " depressed color="error" @click="cancel">Cancelar assinatura</v-btn>     
-      
+    <div v-if="$auth.user.isSubscribed">
+      <v-btn
+        v-if="$auth.user.cancelled && $auth.user.onGracePeriod"
+        depressed
+        color="success"
+        @click="reactivate"
+        :disabled="disabled"
+      >
+        <template v-if="disabled">
+          <v-progress-circular
+            :width="3"
+            :size="20"
+            color="primary"
+            indeterminate
+          ></v-progress-circular>
+          Reativando...
+        </template>
+        <template v-else>
+          <div>Reativar assinatura</div>
+        </template></v-btn
+      >
+      <v-btn
+        v-else-if="!$auth.user.cancelled"
+        depressed
+        color="error"
+        @click="cancel"
+        :disabled="disabled"
+      >
+        <template v-if="disabled">
+          <v-progress-circular
+            :width="3"
+            :size="20"
+            color="primary"
+            indeterminate
+          ></v-progress-circular>
+          Cancelando...
+        </template>
+        <template v-else>
+          <div>Cancelar assinatura</div>
+        </template>
+      </v-btn>
+    </div>
+
+    <div v-else>
+      <v-btn depressed color="primary" :to="{ name: 'planos' }">Assinar</v-btn>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    middleware: "auth",
-    methods: {
-        async cancel({auth}) {
-            const response = await this.$axios.$get('/subscriptions/cancel');
-            await this.$auth.fetchUser()
-        },
+  middleware: "auth",
+  data: () => ({
+    disabled: false,
+  }),
+  methods: {
+    async cancel() {
+      this.disabled = true;
+      const response = await this.$axios.$get("/subscriptions/cancel");
+      await this.$auth.fetchUser();
+      this.disabled = false;
+    },
 
-        async reactivate({auth}) {
-            const response = await this.$axios.$get('/subscriptions/reactivate');
-            await this.$auth.fetchUser()
-        }
-    }
-
-}
+    async reactivate() {
+      this.disabled = true;
+      const response = await this.$axios.$get("/subscriptions/reactivate");
+      await this.$auth.fetchUser();
+      this.disabled = false;
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
